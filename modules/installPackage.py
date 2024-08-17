2bcfdc888a75f7fda480f3547f94ef1d22fe374a
serii <seriiburduja@gmail.com>
2024-08-17 16:26:23 +0300

    upd: pacman to file

--- modules/installPackage.py
+++ modules/installPackage.py
@@ -1,4 +1,6 @@
 import os
+
+from modules.exportToFile import exportToFile
 def installPackage(pacman = True, prop_package = ''):
     if prop_package:
         package = prop_package
@@ -6,6 +8,8 @@ def installPackage(pacman = True, prop_package = ''):
         package = input("Enter package name: ")
     if pacman:
         os.system(f"sudo pacman -S {package}")
+        exportToFile(pacman)
     else:
         os.system(f"yay -S {package}")
+        exportToFile(pacman)
 
