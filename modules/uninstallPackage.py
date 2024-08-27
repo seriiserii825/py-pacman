bd665321801b9a6d04b3dbbef06d07358f77cc7b
serii burduja <seriiburduja@gmail.com>
2024-08-27 11:09:49 +0300

    feat: uninstall package finished

--- modules/uninstallPackage.py
+++ modules/uninstallPackage.py
@@ -1,23 +1,24 @@
 import os
 from pyfzf.pyfzf import FzfPrompt
 
-from modules.getPackageFilePath import getPackageFilePath
-from modules.removePackageFromFile import removePackageFromFile
+from config import CONFIG
+from modules.saveInstalledPackagesToFiles import saveInstalledPackagesToFiles
 fzf = FzfPrompt()
 user = os.getlogin()
 def uninstallPackage():
-    packages_from_yay = []
-    with open(getPackageFilePath(True), "r") as f:
+    saveInstalledPackagesToFiles()
+    with open(CONFIG.INSTALLED_PACMAN_PATH, "r") as f:
         lines = f.readlines()
     # remove the newline character
     lines = [line.strip() for line in lines]
     packages_from_pacman = lines
-    with open(getPackageFilePath(False), "r") as f:
+
+    with open(CONFIG.INSTALLED_YAY_PATH) as f:
         lines = f.readlines()
-    # remove the newline character
     lines = [line.strip() for line in lines]
     packages_from_yay = lines
     packages = packages_from_pacman + packages_from_yay
+
     if not packages:
         print("[red]No packages to uninstall")
         return
@@ -27,7 +28,5 @@ def uninstallPackage():
     print(f"Uninstalling {package[0]}")
     if package[0] in packages_from_pacman:
         os.system(f"sudo pacman -R {package[0]}")
-        removePackageFromFile(getPackageFilePath(True), package[0])
     else:
         os.system(f"yay -R {package[0]}")
-        removePackageFromFile(getPackageFilePath(False), package[0])
