e1e6c8bcda3191e95bd144ca27414f419809745b
serii burduja <seriiburduja@gmail.com>
2024-08-27 12:37:45 +0300

    feat: delete some unusage files

--- modules/showPackages.py
+++
@@ -1,7 +0,0 @@
-import os
-def showPackages(pacman = True):
-    if pacman:
-        os.system(f"pacman -Qe | awk '{{print $1}}' | less")
-    else:
-        # find packages installed with yay
-        os.system(f"pacman -Qm | awk '{{print $1}}' | less")
